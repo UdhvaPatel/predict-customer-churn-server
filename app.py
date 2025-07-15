4c2eda9b10d81218e9617b62dda4447601ea2ea0
UdhvaPatel <120543667+UdhvaPatel@users.noreply.github.com>
2025-07-15 16:40:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_cors import CORS
 import joblib
 import pandas as pd
 
+import os
+
 app = Flask(__name__)
 CORS(app)
 
@@ -26,6 +28,9 @@ def predict():
 
     return jsonify({"churn": int(prediction), "probability": round(probability, 2)})
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT dynamically
+    app.run(host="0.0.0.0", port=port)
+
 
